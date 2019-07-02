f6b240f78753e58ce2b6875b1262c772472331b1
anuchit yaiin <anuchit.im@gmail.com>
2019-07-02 14:50:17 +0700

    first commit

---
+++ wp-frontend.php
@@ -0,0 +1,25 @@
+<?php
+/*
+Plugin Name: WPFrontend
+Plugin URI: https://wordpress.org/plugins/login-recaptcha/
+Description: Plugin frontend
+Author: Anuchit Yai-in
+Version: 1.6.4
+Author URI: https://github.com/cyberscribe/login-recaptcha
+Text Domain: login-recaptcha
+Domain Path: /languages/
+*/
+
+if ( !function_exists( 'add_action' ) ) {
+    die();
+}
+
+class WPFrontend {
+
+    public static function init() {
+
+
+    }
+
+}
+WPFrontend::init();
