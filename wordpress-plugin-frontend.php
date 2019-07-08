115642eb4d0cfe0e4f144b66931d73961096f532
anuchit yaiin <anuchit.im@gmail.com>
2019-07-08 15:07:43 +0700

    update

---
+++ wordpress-plugin-frontend.php
@@ -0,0 +1,19 @@
+<?php
+/*
+Plugin Name: WordPress Frontend
+Plugin URI: https://github.com/anuchit33/wordpress-plugin-frontend
+Description: wordpress-plugin-frontend
+Author: Anuchit Yai-in
+Version: 0.0.1
+Author URI: https://github.com/anuchit33/wordpress-plugin-frontend
+*/
+
+class WPFrontend {
+
+    public static function init() {
+
+    }
+
+}
+
+WPFrontend::init();
