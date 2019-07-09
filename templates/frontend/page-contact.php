869b4bd8b1452a202145ed8779fc6456ce53df95
anuchit yaiin <anuchit.im@gmail.com>
2019-07-09 13:26:10 +0700

    update

--- templates/frontend/page-contact.php
+++ templates/frontend/page-contact.php
@@ -1,10 +1,10 @@
 <?php
 
 # wp_enqueue_script
-wp_enqueue_script('equiz', '/wp-content/plugins/wordpress-plugin-frontend/js/applications.js', array('jquery'), 1.1, true);
+wp_enqueue_script('equiz', '/wp-content/plugins/wordpress-plugin-frontend/inc/js/applications.js', array('jquery'), 1.1, true);
 
 # wp_enqueue_style
-wp_enqueue_style('style-css', '/wp-content/plugins/wordpress-plugin-frontend/css/style.css');
+wp_enqueue_style('style-css', '/wp-content/plugins/wordpress-plugin-frontend/inc/css/style.css');
 
 ?>
 <form method="POST">
@@ -18,5 +18,5 @@ wp_enqueue_style('style-css', '/wp-content/plugins/wordpress-plugin-frontend/css
     <label for="message">Message</label>
     <textarea class="form-control" id="message"  name="message" rows="3" placeholder="Message..."></textarea>
   </div>
-  <button type="submit" class="btn btn-primary">Submit</button>
+  <button type="submit" class="btn btn-primary btn-submit-contact" >Submit</button>
 </form>
